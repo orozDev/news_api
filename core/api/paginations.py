87f436be4908238f449838d02275490f348c65d9
Orozbek Zhenishbek uulu <88781324+orozDev@users.noreply.github.com>
2024-12-03 17:19:38 +0600

    Update paginations.py

--- core/api/paginations.py
+++ core/api/paginations.py
@@ -3,13 +3,13 @@ from rest_framework.pagination import PageNumberPagination
 
 class LargeResultsSetPagination(PageNumberPagination):
     page_size = 1000
-    page_size_query_param = 'limit'
-    page_query_param = 'offset'
+    page_size_query_param = 'page_size'
+    page_query_param = 'page'
     max_page_size = 10000
 
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 12
-    page_size_query_param = 'limit'
-    page_query_param = 'offset'
+    page_size_query_param = 'page_size'
+    page_query_param = 'page'
     max_page_size = 100
